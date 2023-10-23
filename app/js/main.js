8ea015fdf7e4592713d5b07562f866ea50124be1
Koka M <kokamerabishvili@gmail.com>
2023-10-24 03:24:53 +0400

    create header&burger-menu + responsive

---
+++ app/js/main.js
@@ -0,0 +1,20 @@
+// Burger menu
+const menuBtn = document.querySelector("#menu-btn");
+const burgerMenu = document.querySelector(".burger-menu");
+const overlay = document.querySelector(".overlay");
+const burgerMenuCloseBtn = document.querySelector(".burger-menu__btn-close");
+
+menuBtn.addEventListener("click", function () {
+  burgerMenu.classList.add("show");
+  overlay.classList.add("show");
+});
+
+burgerMenuCloseBtn.addEventListener("click", function () {
+  burgerMenu.classList.remove("show");
+  overlay.classList.remove("show");
+});
+
+overlay.addEventListener("click", function () {
+  burgerMenu.classList.remove("show");
+  overlay.classList.remove("show");
+});
